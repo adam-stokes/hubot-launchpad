1ea3cf46a25f6aeecdb6e7fb1566419863744ecd
Adam Stokes <adam.stokes@ubuntu.com>
2015-05-12 16:14:02 -0400

    fix bugid
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- scripts/launchpad.js
+++ scripts/launchpad.js
@@ -15,28 +15,29 @@
 // Author:
 //   Adam Stokes <adam.stokes@ubuntu.com>
 
-function printBug(robot, bug) {
+function printBug(robot, msg, bug) {
   var lpUrl = 'https://api.launchpad.net/1.0/bugs/' + bug;
+  bugOut = {};
 
-  robot.http(lp_url)
+  robot.http(lpUrl)
     .get()(function (err, res, body) {
       var content;
       if ((err !== null) || res.statusCode !== 200) {
-        return msg.send("Unable to parse bug " + bugId + ", try again Señor.");
+        return msg.send("Unable to parse bug " + bug + ", try again Señor.");
       }
       content = JSON.parse(body);
-      bug.id = content.id;
-      bug.title = content.title;
-      bug.web_link = content.web_link;
+      bugOut.id = content.id;
+      bugOut.title = content.title;
+      bugOut.web_link = content.web_link;
       return robot.http(content.bug_tasks_collection_link)
         .get()(function (err, res, body) {
           var task;
           task = JSON.parse(body)
             .entries[0];
-          bug.status = task.status;
-          bug.display_name = task.bug_target_display_name;
-          bug.importance = task.importance;
-          return msg.send((bug.display_name + "/" + bug.title) + (" (" + bug.status + "/" + bug.importance + ") ") + ("[http://pad.lv/" + bug.id + "]"));
+          bugOut.status = task.status;
+          bugOut.display_name = task.bug_target_display_name;
+          bugOut.importance = task.importance;
+          return msg.send((bugOut.display_name + "/" + bugOut.title) + (" (" + bugOut.status + "/" + bugOut.importance + ") ") + ("[http://pad.lv/" + bugOut.id + "]"));
         });
     });
 
@@ -44,12 +45,12 @@ function printBug(robot, bug) {
 
 module.exports = function (robot) {
   robot.hear(/(bug|\#|LP|lp)\s*?(\d{6,})/i, function (msg) {
-    var bugId = msg.match[1];
-    printBug(robot, bugId);
+    var bugId = msg.match[2];
+    printBug(robot, msg, bugId);
   });
 
   robot.hear(/(https:\/\/(?:bugs\.)?launchpad\.net\/.*\/)(\d{6,})/i, function (msg) {
-    var bugId = msg.match[1];
-    printBug(robot, bugId);
+    var bugId = msg.match[2];
+    printBug(robot, msg, bugId);
   });
 };
